fd0755dee9ce51ffb8ea1d6c4f7a49385823668e
nonny <nonnyesodo@gmail.com>
2022-12-10 04:42:06 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - main block
+ *description:get a random with 5
+ *Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
