a9a3a6feda744bac408bd60fd6dca089f1e51188
nonny <nonnyesodo@gmail.com>
2022-12-11 09:23:43 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - main block
+*description:prints the lowercase alphabet in reverse
+*followed by a new line
+*Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha ='z' ; alpha >= 'a' ; alpha --)
+	{
+		putchar(alpha);
+	}
+	
+	putchar('\n');
+	return (0);
+
+}
