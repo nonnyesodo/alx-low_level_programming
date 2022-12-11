565f0e22e7470be73c2e8b134fd6aaf3a2ed3313
nonny <nonnyesodo@gmail.com>
2022-12-11 03:56:19 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - main block
+*description:prints all single digit numbers of base 10 starting from 0
+*followed by a new line
+*Return: 0
+ */
+int main(void)
+{
+	char c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++
+	}
+
+	putchar('\n');
+	return (0);
+}
