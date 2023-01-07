eb445400b5de86ee9fd1dcd60417a43c1843166e
nonny <nonnyesodo@gmail.com>
2023-01-06 17:27:25 -0800

    libmy.a

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+ 
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+ 
+	return (longi);
+}
+ 
+ 
+ 
+ 
+ 
+2-strncpy.c
+ 
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+ 
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+ 
+	return (dest);
+}
+
