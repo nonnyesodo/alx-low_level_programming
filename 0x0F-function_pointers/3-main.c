bc23138c4397c7753d7fa3a8dd9971ddcc538912
nonny <nonnyesodo@gmail.com>
2023-01-18 16:27:32 -0800

    simple operation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
-* main - Prints the result of simple operations
-** @argc: The number of arguments supplied to the program.
-* @argv: An array of pointers to the arguments.
-*
-* Return: Always 0.
-*/
+ * main - Prints the result of simple operations
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
@@ -17,7 +17,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	{
 		printf("Error\n");
 		exit(98);
-		}
+	}
 
 
 	num1 = atoi(argv[1]);
@@ -27,21 +27,20 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
-			printf("Error\n");
-			exit(99);
+		printf("Error\n");
+		exit(99);
 	}
 
 
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
-			printf("Error\n");
-			exit(100);
+	        printf("Error\n");
+		exit(100);
 	}
 
 
-	printf("%d\n", get_op_func(op)(num1, num2));
+	        printf("%d\n", get_op_func(op)(num1, num2));
 
 
-	return (0)
+	        return (0)
 }
