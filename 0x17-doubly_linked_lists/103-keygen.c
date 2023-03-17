cfb1f672c9dcf7fbd3e046faa92221f3c05df0d4
esodo john <nonnyesodo@gmail.com>
2023-03-16 17:51:12 -0700

    0x17. C - Doubly linked lists

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * f4 - finds the biggest number
+ *
+ * @usrn: username
+ * @len: length of username
+ * Return: the biggest number
+ */
+int f4(char *usrn, int len)
+{
+	int ch;
+	int vch;
+	unsigned int rand_num;
+
+	ch = *usrn;
+	vch = 0;
+
+	while (vch < len)
+	{
+		if (ch < usrn[vch])
+			ch = usrn[vch];
+		vch += 1;
+	}
+
+	srand(ch ^ 14);
+	rand_num = rand();
+
+	return (rand_num & 63);
+}
+
+/**
+ * f5 - multiplies each char of username
+ *
+ * @usrn: username
+ * @len: length of username
+ * Return: multiplied char
+ */
+int f5(char *usrn, int len)
+{
+	int ch;
+	int vch;
+
+	ch = vch = 0;
+
+	while (vch < len)
+	{
+		ch = ch + usrn[vch] * usrn[vch];
+		vch += 1;
+	}
+
+	return (((unsigned int)ch ^ 239) & 63);
+}
+
+/**
+ * f6 - generates a random char
+ *
+ * @usrn: username
+ * Return: a random char
+ */
+int f6(char *usrn)
+
